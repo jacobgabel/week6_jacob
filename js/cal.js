06c70fabbbcff800757c68dfce5b3b167ae4d0b3
Gabel <j_gabel@tss-hr420-dm11.fc.ca>
2018-06-13 14:00:09 -0400

    added listener

--- js/cal.js
+++ js/cal.js
@@ -8,13 +8,18 @@
 			var num2 = document.querySelector("#input2");
 			var equals = document.querySelector("#trigger");
 			var outcome = document.querySelector("#value");
-			console.log(num1);
-			console.log(num2);
-			console.log(equals);
-			console.log(outcome);
-		//
+			//console.log(num1);
+			//console.log(num2);
+			//console.log(equals);
+			//console.log(outcome);
+		//Functions
+		function addStuff() {
+			console.log("from addStuff")
+		}
 
-		//
+		//Listeners
+		equals.addEventListener("click", addStuff,
+			false);
 
 
 
